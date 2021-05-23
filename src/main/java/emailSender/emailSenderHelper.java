f6e25380d1664a9194e0b65894b68e55308a14bc
Arnav.Malhotra@viacomcontractor.com <Arnav.Malhotra@viacomcontractor.com>
2021-05-23 17:33:16 +0530

    edit

--- src/main/java/emailSender/emailSenderHelper.java
+++ src/main/java/emailSender/emailSenderHelper.java
@@ -245,8 +245,8 @@ public class emailSenderHelper {
 		properties.put("mail.smtp.starttls.enable", "true");
 		properties.put("mail.smtp.host", "smtp.gmail.com");
 		properties.put("mail.smtp.port", "587");
-		properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
-		properties.put("mail.smtp.starttls.enable", "true");
+		//properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+		//properties.put("mail.smtp.starttls.enable", "true");
 		return properties;
 	}
 	
